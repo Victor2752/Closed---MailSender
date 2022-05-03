2a4a51574f8fc4e3dfd38f6cef25ecd42aa85715
Victor2752 <victor2752@hotmail.com>
2022-05-03 11:30:06 -0300

    aula 1
    criado o botão e adicionado algumas bibliotecas...

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257e5'
+        },
+      },
+    },
+  },
+  plugins: [],
+}
