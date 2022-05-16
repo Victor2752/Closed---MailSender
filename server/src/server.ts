088c8c2ddb97aa30e7b1aa541f39479afb152a50
Victor2752 <victor2752@hotmail.com>
2022-05-16 17:10:26 -0300

    correção erro $PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,12 @@ import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
+app.listen(PORT, () => {
     console.log(`HTTP server running! on PORT ${process.env.PORT}`);
 })
