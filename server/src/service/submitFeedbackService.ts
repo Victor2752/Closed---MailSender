1c31e06d05f110c238a981eed611eab9934aa5e9
Victor2752 <victor2752@hotmail.com>
2022-05-16 16:32:44 -0300

    ligação web + backend
    ainda no meio da aula 5 e testando algumas coisas

--- server/src/service/submitFeedbackService.ts
+++ server/src/service/submitFeedbackService.ts
@@ -40,7 +40,7 @@ export class SubmitFeedbackUseCase {
                 `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
                     `<p>Tipo de Feedback: ${type}</p>`,
                     `<p>Comentário: ${comment}</p>`,
-                    screenshot && `<img src="${screenshot}" />`,
+                    screenshot ? `<img src="${screenshot}" />` : ``,
                 `</div>`
                 ].join('\n')
         })
