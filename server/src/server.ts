476288b5abe0ae3720fb8a6e62ddc2c76af749a2
Victor2752 <victor2752@hotmail.com>
2022-05-16 17:20:42 -0300

    nova tentativa correção erro port

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,11 @@ import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
-const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`HTTP server running! on PORT ${process.env.PORT}`);
+app.listen(process.env.PORT || 3333, () => {
+    console.log('HTTP server running!');
 })
