a6938271ab09983b8e4737820fbc5b7f02fbf883
Victor2752 <victor2752@hotmail.com>
2022-05-17 14:03:41 -0300

    Update routes.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,21 +10,26 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
 
-    const prismaFeedbackRepository = new PrismaFeedbackRepository()
-    const nodemailMailAdapater = new NodemailMailAdapater()
+    try {
+        const prismaFeedbackRepository = new PrismaFeedbackRepository()
+        const nodemailMailAdapater = new NodemailMailAdapater()
+    
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbackRepository,
+            nodemailMailAdapater
+        )
+    
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot,
+        })
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbackRepository,
-        nodemailMailAdapater
-    )
+        return res.status(201).send()
+    } 
+    catch (err) {
+        console.error(err);
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot,
-    })
-
-
-
-    return res.status(201).send()
+        return res.status(500).send();
+    }
 })
